b3953f883efc4dd784e676e4b43e8ae42093bf2b
Alex <alancast@umich.edu>
2015-07-23 11:39:12 -0500

    working on UI

--- FreeFlow/ViewController.swift
+++ FreeFlow/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,10 +20,45 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
     var order_types = ["Buy", "Sell"]
+    var price_types = ["Market", "Limit", "Stop Loss", "Stop Limit"]
+    var ticker = "AAPL"
+    var num_shares = 0
+    var duration = ["Today"]
+    var order_qualifier = ["None", "All or None", "Fill or Kill"]
+    private var order_type_component = 0
+    private var price_type_component = 1
+    private var order_qualifier_component = 2
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
     
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return order_types.count
+        if pickerView.restorationIdentifier == "order_types"{
+            return order_types.count
+        }
+        else if pickerView.restorationIdentifier == "price_types"{
+            return price_types.count
+        }
+        else if pickerView.restorationIdentifier == "order_qualifier"{
+            return order_qualifier.count
+        }
+        return duration.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String!{
+        if pickerView.restorationIdentifier == "order_types"{
+            return order_types[row]
+        }
+        else if pickerView.restorationIdentifier == "price_types"{
+            return price_types[row]
+        }
+        else if pickerView.restorationIdentifier == "order_qualifier"{
+            return order_qualifier[row]
+        }
+        return duration[row]
     }
 
 
